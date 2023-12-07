71b93949d43e2f3fe67aec81744a4ee348da247c
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-07 10:58:50 -0300

    Ok

---
+++ StarCapEFModel/DAL/BebidaDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCapEFModel.DAL
+{
+    public class BebidaDAL
+    {
+        public dbstarcapefwebEntities db = new dbstarcapefwebEntities();
+
+        public List<Bebida> GetAll()
+        {
+            return db.Bebida.ToList();
+        }
+    }
+}
---
+++ StarCapEFModel/DAL/ClienteDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarCapEFModel.DAL
+{
+    public class ClienteDAL
+    {
+        public dbstarcapefwebEntities db = new dbstarcapefwebEntities();
+
+        public void Save(Cliente c)
+        {
+            db.Cliente.Add(c);
+            db.SaveChanges();
+        }
+
+        public void Delete(String rut)
+        {
+            Cliente c = db.Cliente.Find(rut);
+            db.Cliente.Remove(c);
+            db.SaveChanges();
+        }
+
+        public List<Cliente> GetAll()
+        {
+            return db.Cliente.ToList();
+        }
+
+        public List<Cliente> GetAll(int nivel)
+        {
+            var query = from c in db.Cliente where c.Nivel == nivel select c;
+            return query.ToList();
+        }
+    }
+}
